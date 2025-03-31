97dd443b7c7fdf39dfa072fda9ef00769aac2916
jshumphreysss <134986689+jshumphreysss@users.noreply.github.com>
2025-03-31 19:41:53 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,15 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Press Start 2P Font */}
         <link
           href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
           rel="stylesheet"
         />
       </Head>
-      <body className="bg-gray-200 text-black font-press-start">
+      <body>
         <Main />
         <NextScript />
       </body>
